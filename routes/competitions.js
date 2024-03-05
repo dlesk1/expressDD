8e4bb25b2b5d1b33510f6fb43e8f3426bdbb5ef7
dlesk1 <dleskdomagoj@gmail.com>
2024-03-05 09:23:47 +0100

    .

--- routes/competitions.js
+++ routes/competitions.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { authRequired, adminRequired } = require("../services/auth.js");
 const Joi = require("joi");
 const { db } = require("../services/db.js");
+const { decode } = require("jsonwebtoken");
 
 // GET /competitions
 router.get("/", authRequired, function (req, res, next) {
@@ -17,7 +18,7 @@ router.get("/", authRequired, function (req, res, next) {
     res.render("competitions/index", { result: { items: result } });
 });
 
-// SCHEMA signup
+// SCHEMA id
 const schema_id = Joi.object({
     id: Joi.number().integer().positive().required()
 });
@@ -36,7 +37,6 @@ router.get("/delete/:id", adminRequired, function (req, res, next) {
     if (!deleteResult.changes || deleteResult.changes !== 1) {
         throw new Error("Operacija nije uspjela");
     }
-
     res.redirect("/competitions");
 });
 
@@ -58,45 +58,16 @@ router.get("/edit/:id", adminRequired, function (req, res, next) {
     res.render("competitions/form", { result: { display_form: true, edit: selectResult } });
 });
 
-// SCHEMA edit
-const schema_edit = Joi.object({
-    id: Joi.number().integer().positive().required(),
-    name: Joi.string().min(3).max(50).required(),
-    description: Joi.string().min(3).max(1000).required(),
-    apply_till: Joi.date().iso().required()
-});
-
-// POST /competitions/edit
-router.post("/edit", authRequired, function (req, res, next) {
-    // do validation
-    const result = schema_edit.validate(req.body);
-    if (result.error) {
-        res.render("competitions/form", { result: { validation_error: true, display_form: true } });
-        return;
-    }
-
-    const stmt = db.prepare("UPDATE competitions SET name = ?, description = ?, apply_till = ? WHERE id = ?;");
-    const updateResult = stmt.run(req.body.name, req.body.description, req.body.apply_till, req.body.id);
-
-    if (updateResult.changes && updateResult.changes === 1) {
-        res.redirect("/competitions");
-    } else {
-        res.render("competitions/form", { result: { database_error: true } });
-    }
-});
-
 // GET /competitions/add
 router.get("/add", adminRequired, function (req, res, next) {
     res.render("competitions/form", { result: { display_form: true } });
 });
-
 // SCHEMA add
 const schema_add = Joi.object({
     name: Joi.string().min(3).max(50).required(),
     description: Joi.string().min(3).max(1000).required(),
     apply_till: Joi.date().iso().required()
 });
-
 // POST /competitions/add
 router.post("/add", adminRequired, function (req, res, next) {
     // do validation
@@ -115,5 +86,30 @@ router.post("/add", adminRequired, function (req, res, next) {
         res.render("competitions/form", { result: { database_error: true } });
     }
 });
+// SCHEMA edit
+const schema_edit = Joi.object({
+    id: Joi.number().integer().positive().required(),
+    name: Joi.string().min(3).max(50).required(),
+    description: Joi.string().min(3).max(1000).required(),
+    apply_till: Joi.date().iso().required()
+});
+// POST /competitions/edit/
+router.post("/edit", adminRequired, function (req, res, next) {
+    // do validation
+    const result = schema_edit.validate(req.body);
+    if (result.error) {
+        res.render("competitions/form", { result: { validation_error: true, display_form: true } });
+        return;
+    }
+    const stmt = db.prepare("UPDATE competitions SET name = ?, description = ?, apply_till = ? WHERE id = ?;");
+    const updateResult = stmt.run(req.body.name, req.body.description, req.body.apply_till, req.body.id);
+    if (updateResult.changes && updateResult.changes === 1) {
+        res.redirect("/competitions");
+    } else {
+        res.render("competitions/form", { result: { database_error: true } });
+    }
+});
+
+
 
 module.exports = router;
